9bf7c79af30e3111a432901d221e4d217f57a109
Rogerio Oliveira <rogerio@versatility.com.br>
2023-04-17 00:01:06 +0200

    - wip; fix the way we pass the store;

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -11,11 +11,10 @@ pub fn login_route() -> impl Filter<Extract = (impl Reply,), Error = Rejection> 
 }
 
 pub fn registration_route(store: Store) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
-    let store_filter = warp::any().map(move || store.clone());
     warp::post()
         .and(path("registration"))
         .and(path::end())
-        .and(store_filter.clone())
+        .and(warp::any().map(move || store.clone()))
         .and(warp::body::json())
         .and_then(register_account)
 }
