d82280db816ecacadc272c68160e99a1f3a8bdbb
Rogerio Oliveira <rogerio@versatility.com.br>
2023-04-26 18:28:40 +0200

    - added Session model to use when user is logged; - added new error in account; - added auth and verify_token in authentication controller;

--- src/controllers/authentication.rs
+++ src/controllers/authentication.rs
@@ -1,11 +1,17 @@
-use std::{sync::Arc, env};
 use argon2::Config;
 use chrono::Utc;
 use paseto::PasetoBuilder;
 use rand::Rng;
-use warp::{Reply, Rejection, hyper::StatusCode, Future, reply::json, reject::custom};
+use std::{env, sync::Arc, future};
+use warp::{
+    header, hyper::StatusCode, reject::custom, reply::json, Filter, Future, Rejection, Reply,
+};
 
-use crate::{models::account::{Account, AccountId}, store::Store, custom_errors::account::Error};
+use crate::{
+    custom_errors::account::Error,
+    models::account::{Account, AccountId, Session},
+    store::Store,
+};
 
 #[derive(Debug, Clone)]
 pub struct AuthenticationController {
@@ -20,13 +26,14 @@ impl AuthenticationController {
     // The Future type itself requires a lifetime parameter to be specified, which is used to specify the
     // lifetime of the returned Future.
     // Note that naming the lifetime parameter explicitly can make the code easier to read and understand,
-    // but it is not necessary in this case because the anonymous lifetime '_ can be used to elide the 
+    // but it is not necessary in this case because the anonymous lifetime '_ can be used to elide the
     // lifetime parameter and let the Rust compiler infer the lifetime automatically.
-    pub fn register_account(&self, account: Account)
-        -> impl warp::Future<Output = Result<impl Reply, Rejection>> + Send + '_ {
-    
+    pub fn register_account(
+        &self,
+        account: Account,
+    ) -> impl warp::Future<Output = Result<impl Reply, Rejection>> + Send + '_ {
         let hashed_password = self.hash_password(account.password.as_bytes());
-    
+
         let account = Account {
             id: account.id.to_owned(),
             email: account.email.to_owned(),
@@ -40,26 +47,39 @@ impl AuthenticationController {
             }
         }
     }
-    
-    pub fn login(&self, login: Account)
-        -> impl Future<Output = Result<impl Reply, Rejection>> + Send + '_ {
+
+    pub fn login(
+        &self,
+        login: Account,
+    ) -> impl Future<Output = Result<impl Reply, Rejection>> + Send + '_ {
         async move {
             match self.repository.get_account(&login.email).await {
-                Ok(account) => match self.verify_password(&account.password, login.password.as_bytes()) {
-                    Ok(verified) => {
-                        if verified {
-                            Ok(json(&self.issue_token(
-                                account.id.expect("Account Id not found")
-                            )))
-                        } else {
-                            Err(custom(Error::WrongCredentials))
+                Ok(account) => {
+                    match self.verify_password(&account.password, login.password.as_bytes()) {
+                        Ok(verified) => {
+                            if verified {
+                                Ok(json(
+                                    &self.issue_token(account.id.expect("Account Id not found")),
+                                ))
+                            } else {
+                                Err(custom(Error::WrongCredentials))
+                            }
                         }
-                    },
-                    Err(e) => Err(custom(Error::ArgonLibraryError(e))),
-                },
+                        Err(e) => Err(custom(Error::ArgonLibraryError(e))),
+                    }
+                }
                 Err(e) => Err(custom(e)),
             }
         }
+    }
+
+    pub fn auth() -> impl Filter<Extract = (Session,), Error = Rejection> + Clone {
+        header::<String>("Authorization").and_then(|token: String| {
+            match Self::verify_token(token) {
+                Ok(t) => return future::ready(Ok(t)),
+                Err(_) => return future::ready(Err(warp::reject::custom(Error::Unauthorized))),
+            }
+        })
     }
 
     fn hash_password(&self, password: &[u8]) -> String {
@@ -76,7 +96,7 @@ impl AuthenticationController {
         let key = env::var("PASETO_KEY").unwrap();
         let current_date_time = Utc::now();
         let dt = current_date_time + chrono::Duration::days(1);
-    
+
         PasetoBuilder::new()
             .set_encryption_key(&Vec::from(key.as_bytes()))
             .set_expiration(&dt)
@@ -86,4 +106,17 @@ impl AuthenticationController {
             .expect("Failed to construct paseto token w/ builder!")
     }
 
+    fn verify_token(token: String) -> Result<Session, Error> {
+        let key = env::var("PASETO_KEY").expect("please define the PASETO_KEY env variable");
+        let token = paseto::tokens::validate_local_token(
+            &token,
+            None,
+            key.as_bytes(),
+            &paseto::tokens::TimeBackend::Chrono,
+        )
+        .map_err(|_| crate::custom_errors::account::Error::CannotDecryptToken)?;
+
+        serde_json::from_value::<Session>(token)
+            .map_err(|_| crate::custom_errors::account::Error::CannotDecryptToken)
+    }
 }
--- src/models/account.rs
+++ src/models/account.rs
@@ -1,3 +1,4 @@
+use chrono::{Utc, DateTime};
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -9,3 +10,10 @@ pub struct Account {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct AccountId(pub i32);
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Session {
+    pub exp: DateTime<Utc>,
+    pub account_id: AccountId,
+    pub nbf: DateTime<Utc>,
+}
